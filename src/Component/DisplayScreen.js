752db5b8a772120c13fce19fd4fe04a4894ce02e
jhetpatel <hetalpatel0976@gmail.com>
2020-01-14 11:18:43 -0800

    Video autoplay set to true

--- src/Component/DisplayScreen.js
+++ src/Component/DisplayScreen.js
@@ -10,13 +10,12 @@ class DisplayScreen extends Component {
     render() {
         return (
             <div className="display_area">
-                {/* <img src={hotelfront_img} alt="hotel" style={{width:"85%",borderRadius:"20px"}}></img> */}
-                <video loop autoPlay playsInline preload="true" controls className="display_video">
+                <video loop autoPlay="true" playsInline preload="true" controls className="display_video">
                     <source src={tunnel} />
-                    <source src='http://techslides.com/demos/sample-videos/small.webm' type='video/webm' />>
-                        <source src='http://techslides.com/demos/sample-videos/small.ogv' type='video/ogg' />>
-                        <source src='http://techslides.com/demos/sample-videos/small.mp4' type='video/mp4' />>
-                        <source src='http://techslides.com/demos/sample-videos/small.3gp' type='video/3gp' />>
+                    {/* <source src='http://techslides.com/demos/sample-videos/small.webm' type='video/webm' />
+                    <source src='http://techslides.com/demos/sample-videos/small.ogv' type='video/ogg' />
+                    <source src='http://techslides.com/demos/sample-videos/small.mp4' type='video/mp4' />
+                    <source src='http://techslides.com/demos/sample-videos/small.3gp' type='video/3gp' /> */}
                 </video>
             </div>
 
